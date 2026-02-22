fb2d0f0e422278776bbb3c2ee056be58ac7f836d
pravallikareddyreddygari <reddygaripravallikareddy@gmail.com>
2026-02-22 18:58:44 +0530

    update owner

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -4,15 +4,10 @@ export default function Footer() {
   return (
     <footer className="text-center py-8 text-white/40 text-sm">
       <p>
-        Made with ❤️ from MindBook team (Team Lead:{" "}
-        <a href="https://www.linkedin.com/in/venugopalpathuri3000/">
-          <b>Venu Gopal</b>
-        </a>
-        , Dev:{" "}
+        Made with ❤️ from MindBook team{" "}
         <a href="https://www.linkedin.com/in/reddygaripravallikareddy/">
-          <b>Pravallika R</b>.
+          <b>Pravallika R</b>
         </a>
-        )
       </p>
     </footer>
   );
